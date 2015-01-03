d6dccdad1c32b05593ab7b74895dee335d8ae098
Brian Hsu <brianhsu.hsu@gmail.com>
2015-01-03 17:39:09 +0800

    Add basic stock adding function.

--- src/main/scala/code/snippet/AddStockForm.scala
+++ src/main/scala/code/snippet/AddStockForm.scala
@@ -11,27 +11,20 @@ import java.text.SimpleDateFormat
 import net.liftweb.http.S
 import net.liftweb.common._
 
-object DateToCalendar {
-  import java.util.Calendar
-  import java.util.Date
-  implicit def apply(date: Date): Calendar = {
-    val calendar = Calendar.getInstance
-    calendar.setTime(date)
-    calendar
-  }
-}
-
-
-class AddGoldForm {
+class AddStockForm {
 
   import DateToCalendar._
 
   private var dateString: String = _
+  private var stockCode: String = _
   private var quantity: Box[Int] = Empty
-  private var price: Box[Int] = Empty
+  private var price: Box[Double] = Empty
+  private var buyFee: Box[Int] = Empty
   private var targetLoose: Box[Int] = Empty
   private var targetEarning: Box[Int] = Empty
   private val dateFormatter = new SimpleDateFormat("yyyy-MM-dd")
+
+  private val allStockCode = Stock.stockTable.map(_.code).toSet
 
   def checkDate(dateString: String): Option[String] = {
     Try(dateFormatter.parse(dateString)) match {
@@ -40,14 +33,21 @@ class AddGoldForm {
     }
   }
 
-  def checkInt(title: String, data: Box[Int]): Option[String] = {
+  def checkInt[T](title: String, data: Box[T]): Option[String] = {
     data match {
       case Full(x) => None
       case _ => Some(s"$title 不是正確的數字，煩請檢查")
     }
   }
 
-  def addGold(): JsCmd = {
+  def checkStockCode(stockCode: String) = {
+    allStockCode.contains(stockCode) match {
+      case true  => None
+      case false => Some("無法找到這個股票，煩請檢查")
+    }
+  }
+
+  def addStock(): JsCmd = {
 
     def saveToDB(): JsCmd = {
       val newRecord = for {
@@ -57,7 +57,7 @@ class AddGoldForm {
         price <- this.price
         targetEarning <- this.targetEarning
         targetLoose <- this.targetLoose
-        newRecord <- GoldInHand.createRecord
+        newRecord <- StockInHand.createRecord
                                .userID(currentUser.id.toString)
                                .buyDate(date)
                                .buyPrice(price)
@@ -70,15 +70,13 @@ class AddGoldForm {
 
       import code.comet._
       newRecord match {
-        case Some(record) => 
-          S.notice("成功新增持有黃金")
-          GoldTable ! GoldTable.UpdateTable
-        case _ => 
-          S.error("無法存檔，請稍候再試")
+        case Some(record) => S.notice("成功新增股票")
+        case _ => S.error("無法存檔，請稍候再試")
       }
     }
 
     val errors = List(
+      checkStockCode(stockCode),
       checkDate(dateString),
       checkInt("持有單位", quantity),
       checkInt("成本單價", price),
@@ -90,14 +88,22 @@ class AddGoldForm {
       case Nil => saveToDB()
       case _   => errors.foreach(S.error)
     }
+   
   }
 
   def render = {
-    "#addGoldDate [onchange]" #> SHtml.onEvent(dateString = _) &
-    "#quantity" #> SHtml.ajaxText("", false, (x: String) => {quantity = asInt(x); Noop}) &
-    "#price" #> SHtml.ajaxText("", false, (x: String) => {price = asInt(x); Noop}) &
-    "#targetLoose" #> SHtml.ajaxText("", false, (x: String) => {targetLoose = asInt(x); Noop}) &
-    "#targetEarning" #> SHtml.ajaxText("", false, (x: String) => {targetEarning = asInt(x); Noop}) &
-    "#addGoldButton" #> SHtml.ajaxOnSubmit(addGold _)
+
+    ".stockListItem" #> Stock.stockTable.map { stockInfo =>
+      ".stockListItem *" #> stockInfo.name &
+      ".stockListItem [value]" #> stockInfo.code
+    } &
+    "#addStockCode [onchange]" #> SHtml.onEvent(stockCode = _) &
+    "#addStockDate [onchange]" #> SHtml.onEvent(dateString = _) &
+    "#addStockQuantity" #> SHtml.ajaxText("", false, (x: String) => {quantity = asInt(x); Noop}) &
+    "#addStockPrice" #> SHtml.ajaxText("", false, (x: String) => {price = asDouble(x); Noop}) &
+    "#addStockFee" #> SHtml.ajaxText("", false, (x: String) => {buyFee = asInt(x); Noop}) &
+    "#addStockTargetLoose" #> SHtml.ajaxText("", false, (x: String) => {targetLoose = asInt(x); Noop}) &
+    "#addStockTargetEarning" #> SHtml.ajaxText("", false, (x: String) => {targetEarning = asInt(x); Noop}) &
+    "#addStockButton" #> SHtml.ajaxOnSubmit(addStock _)
   }
 }
